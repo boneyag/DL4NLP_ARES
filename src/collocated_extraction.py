0dc9dfa4975c2a86d23b2d5984e19db187ed713d
sakib1486 <sakibiut134406@gmail.com>
2021-04-19 02:29:27 +0600

    Removing duplicates in extracted Sentences

--- src/collocated_extraction.py
+++ src/collocated_extraction.py
@@ -21,17 +21,23 @@ def main():
 	ext_sentences = []
 	with open('../data/testCorpora_syntagExtraction.txt') as t:
 		corpus = t.readlines()
-
-		with open('../data/ext1.txt', 'w') as r:
-
-			for c in corpus:
-				print(c)
-				for ele in res_data:
-					if ele[2] in c:
-						if ele[4] in c:
-							r.write(c)
-			r.close()
+		for c in corpus:
+			#print(c)
+			for ele in res_data:
+				if ele[2] in c:
+					if ele[4] in c:
+						ext_sentences.append(c)
 		t.close()
+
+	ext_sentences = list(set(ext_sentences))					
+
+	with open('../data/ext1.txt', 'w') as r:
+		for line in ext_sentences:
+			r.write(line)
+		r.close()
+		
+
+
 
 
 
